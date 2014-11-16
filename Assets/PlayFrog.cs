a9ef550328ade938cfb25909514e05177a67e98a
Nguyễn Xuân Bảo <nxbao.9x@gmail.com>
2014-11-16 15:11:44 +0700

    Create

---
+++ Assets/PlayFrog.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayFrog : MonoBehaviour {
+	private int jumpLong = 500;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump();
+				}
+	}
+	void Jump()
+	{
+		rigidbody.AddForce(new Vector3(0,jumpLong,0),ForceMode.Force);
+	}
+}
